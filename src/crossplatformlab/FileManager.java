1b0051b3c5438f5fadaf32c0e7fa699b4b680ba6
Arthur Sharipov <sharipov@sheridancollege.ca>
2026-02-18 14:31:20 -0500

    Trigger of workflow

--- src/crossplatformlab/FileManager.java
+++ src/crossplatformlab/FileManager.java
@@ -15,6 +15,7 @@ public class FileManager {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Some change to trigger workflow
     }
     
 }
